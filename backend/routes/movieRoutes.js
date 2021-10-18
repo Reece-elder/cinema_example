64dd98d8e54c5693ef47c4aded70d9e2fbeca3bc
reece-elder <reece.elder@qa.com>
2021-10-18 14:14:25 +0100

    connected basic routes

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,11 +1,37 @@
 const router = require('express').Router();
 
-const movie = require("../models/movies");
+const { Movie } = require('../models/movies');
 
 // demo request
 router.get('/movie', (req, res) => {
     console.log("Hey, im a route!!");
     res.send("Hey, this is from the route!")
 })
+
+router.post("/create", (req, res, next) => {
+    // console.log(req);
+
+    const record = new Movie(req.body);
+    console.log(req.body);
+    record.id = 5;
+
+    record.save().then((result) => {
+        res.status(201).send(`${result.id} has been added!`)
+    }).catch((error) => {
+        return next(error);
+    });
+
+});
+
+router.get("/getAll", (req, res, next) => {
+    Movie.find((error, movies) => {
+        if(error) {
+            console.log("Cant get movies");
+            next(error);
+        } else {
+            res.send(movies);
+        };
+    });
+});
 
 module.exports = router;
