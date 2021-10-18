3a5b78b33de173edb3bee1969d2b8ab261757a69
reece-elder <reece.elder@qa.com>
2021-10-18 13:01:48 +0100

    modified files, added listen port

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.listen(4494);
+app.get('/test', (req, res) => {
+    res.send("Hey this is the text!");
+    return console.log("Site has been accessed!");
+})
+
+const server = app.listen(5015, () => {
+    console.log(`Server started on port ${server.address().port}`);
+})
+
+// Exporting the app
+module.exports = server
